a5205e4d302521fd74c4fe9aa61ff7635f81098b
Gabriel-Assis03 <gabrieleboltffggff@gmail.com>
2024-10-18 18:47:33 -0300

    req 8

--- spotnews/urls.py
+++ spotnews/urls.py
@@ -17,10 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from rest_framework import routers
 from django.conf.urls.static import static
+from news_rest.views.category_view import CategoryViewSet
+
+router = routers.DefaultRouter()
+router.register(r'categories', CategoryViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
     path("", include("news.urls")),
 ]
 
