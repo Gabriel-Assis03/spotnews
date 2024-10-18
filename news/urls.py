dda555b142145df226b2b55006f73c55e96f8843
Gabriel-Assis03 <gabrieleboltffggff@gmail.com>
2024-10-18 15:11:06 -0300

    req 5

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, news_details
 
 
 urlpatterns = [
     path("", index, name="home-page"),
+    path("news/<int:id>", news_details, name="news-details-page"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404
 from .models import News
 
 
 def index(request):
     context = {'news': News.objects.all()}
     return render(request, 'home.html', context)
+
+
+def news_details(request, id):
+    try:
+        new = get_object_or_404(News, id=id)
+        return render(request, 'news_details.html', {'new': new})
+    except Http404:
+        return render(request, '404.html')
+    # context = {'new': News.objects.first(id)}
+    # return render(request, 'news_details.html', context)
